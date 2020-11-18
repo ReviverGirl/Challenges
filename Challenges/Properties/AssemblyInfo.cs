7d3cea2fc4617b1d05c1896138dd1ba2fb2237ef
Sabrina Jimenez <ReviverGirl@outlook.com>
2020-11-17 19:02:51 -0500

    inital commit

---
+++ Challenges/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenges")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea80edec-f2fb-4f93-8f51-6610c0d74589")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Challenges/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenges
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string name = "Supercalifragilisticexpialidocious";
+            foreach (char letter in name)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    
+
+    [TestMethod]
+    public void TestMethod2()
+        {
+            string name = "Supercalifragilisticexpialidocious";
+            foreach (char letter in name)
+            {
+                if(letter == 'i')
+                {
+                    Console.WriteLine('i');
+                }
+                else
+                {
+                    Console.WriteLine("Not an i. ");
+                }
+            }
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string name = "Supercalifragilisticexpialidocious";
+            int count = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+        }
